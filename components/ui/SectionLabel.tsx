2436161540d97dc4874220b042cce6d6a6463687
acanserb <acanserb@gmail.com>
2026-09-24 20:31:11 +0300

    Delete components directory

--- components/ui/SectionLabel.tsx
+++
@@ -1,7 +0,0 @@
-export default function SectionLabel({ children }: { children: React.ReactNode }) {
-  return (
-    <span className="text-xs uppercase tracking-[0.28em] text-accent font-medium">
-      {children}
-    </span>
-  );
-}
